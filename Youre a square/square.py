a46016d40e5af31d4313cc6ce7bcaa5cd99785e2
zeuspod <josseluism@gmail.com>
2021-05-28 12:09:33 -0400

    second commit

---
+++ Youre a square/square.py
@@ -0,0 +1,16 @@
+def is_square(n):
+    if n < 0:
+        return False
+    elif n == 0:
+        return True
+    elif n >= 1:
+        square = 2 * n 
+        if square % 2 == 0:
+            return True
+        else:
+            return False
+
+
+n = int(input("Por favor introduce tu numero: "))
+micuadrado = is_square(n)
+print (micuadrado)
