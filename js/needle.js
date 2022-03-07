0c72d020e6b2634b1061c8a7a1530135fb528e03
zeuspod <josseluism@gmail.com>
2022-03-06 23:11:52 -0400

    needje js

---
+++ js/needle.js
@@ -0,0 +1,8 @@
+/* Write a function findNeedle() that takes an array full of junk but containing one "needle"
+ */
+
+
+function findNeedle(haystack){
+    return 'found the needle at position ' + haystack.indexOf('needle');
+
+}
