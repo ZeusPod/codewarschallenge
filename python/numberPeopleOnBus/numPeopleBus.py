0f0922b2a2d4f0967fe59451ec74a6e75c86556e
zeuspod <josseluism@gmail.com>
2022-05-15 15:11:18 -0400

    numpeople fixed

--- python/numberPeopleOnBus/numPeopleBus.py
+++ python/numberPeopleOnBus/numPeopleBus.py
@@ -1,10 +1,12 @@
 import doctest
 
 def number(bus_stops):
-    """ >>> number([[10,0],[3,5],[5,8]])
+    """ 
+    >>> number([[10,0],[3,5],[5,8]])
     5
     >>> number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])
-    17 
+    17
+
     """
 
     large = len(bus_stops)
@@ -15,5 +17,5 @@ def number(bus_stops):
         down += bus_stops[i][1]
     return up_bus - down
 
-
 doctest.testmod()    
+
