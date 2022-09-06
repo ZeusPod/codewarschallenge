9c4f45b24981a663289849a0f6ce980115426c4a
zeuspod <josseluism@gmail.com>
2022-09-05 23:11:24 -0400

    python: remove exclamation add doctest

--- python/remove_exclamation/remove_exc.py
+++ python/remove_exclamation/remove_exc.py
@@ -1,8 +1,16 @@
+import doctest
+
+
 def remove_exclamation_marks(s):
+    """
+    fuction to remove exclamation simbol for any string
+    >>> remove_exclamation_marks('Hello World!')
+    'Hello World'
+    >>> remove_exclamation_marks('Good morning!')
+    'Good morning'
+    """
     return s.replace("!","")
 
 
 
-
-print(remove_exclamation_marks('Hello world!'))
-
+doctest.testmod()
